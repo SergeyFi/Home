648918f618ebee0bf6c5278112330a17f1b23241
Sergey Firsov <fearthunder@yandex.ru>
2020-09-06 20:54:12 +0300

    Init commit

---
+++ Source/Home/HomeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HomeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOME_API AHomeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
